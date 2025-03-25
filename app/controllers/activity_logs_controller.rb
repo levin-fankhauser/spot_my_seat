cfb7d690ff7aada47443be708f967600c8369b7a
levin-fankhauser <levin.fankhauser@gmx.ch>
2025-03-25 14:43:24 +0100

    Implemented authorization, roles and rights

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -1,7 +1,15 @@
 class ActivityLogsController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @post_versions = PaperTrail::Version.where(item_type: "Post").order(created_at: :desc)
     @user_versions = PaperTrail::Version.where(item_type: "User").order(created_at: :desc)
     @seat_versions = PaperTrail::Version.where(item_type: "Seat").order(created_at: :desc)
+  end
+
+  private
+
+  def authenticate_admin!
+    redirect_to root_path, alert: "Access denied." unless current_user&.admin?
   end
 end
